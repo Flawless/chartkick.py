6b4c89392a05f85cfd2f1ac1af8eb4cb4f6030c6
Alexander Ushanov <flawless@last-try.org>
2018-07-10 21:49:14 +0300

    Fixed version comparsion

--- chartkick/ext.py
+++ chartkick/ext.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 import json
 import itertools
 import functools
+from distutils.version import LooseVersiond
 
 import jinja2
 from jinja2 import nodes
@@ -68,7 +69,7 @@ class ChartExtension(Extension):
         options.update(name=name, id=id)
 
         # jinja2 prepends 'l_' or 'l_{{ n }}'(ver>=2.9) to keys
-        if jinja2.__version__ >= '2.9':
+        if LooseVersion(jinja2.__version__) >= LooseVersion('2.9'):
             kwargs = dict((k[4:], v) for (k, v) in kwargs.items())
         else:
             kwargs = dict((k[2:], v) for (k, v) in kwargs.items())
